faf85c9603b2dced73f84c7872b14e57c10834c5
Vladislav Chesnov <tazaqsp@mail.ru>
2025-08-19 21:08:39 +0500

    fix nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
   ssr: false,
   devServer: {
     port: 4898,
@@ -14,5 +17,10 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     clickupApiKey: process.env.NUXT_CLICKUP_API_KEY,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 })
