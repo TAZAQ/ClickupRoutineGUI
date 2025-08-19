c7b47ca6c1cb8c9bf7e9b4ffde3232b58efcb772
Vladislav Chesnov <tazaqsp@mail.ru>
2025-08-19 21:24:04 +0500

    taskIds

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,24 @@
   <div class="flex flex-col">
     <UTextarea v-model="lists"/>
     <UButton @click="onFetchTasks">Получить задачи</UButton>
+
+    <div class="flex gap-1">
+      <UInput
+          class="grow"
+          :value="taskIds"
+          readonly
+          placeholder="Task IDs будут отображены здесь..."
+      />
+      <UButton
+          variant="ghost"
+          size="xs"
+          icon="i-heroicons-clipboard-document"
+          @click="copyToClipboard"
+      >
+        Копировать
+      </UButton>
+    </div>
+
     <span>{{ listIds }}</span>
     <span>{{ tasks.length }}</span>
   </div>
@@ -23,10 +41,22 @@ const listIds = computed(() =>
     .filter(Boolean)
 )
 
+const taskIds = computed(() =>
+  tasks.value.map((task) => task.id).join('|')
+)
+
 function onFetchTasks() {
   listIds.value.forEach(async (listId) => {
     const response = await api.getTasks(listId as string)
     tasks.value.push(...response.data.tasks)
   })
+}
+
+async function copyToClipboard() {
+  try {
+    await navigator.clipboard.writeText(taskIds.value)
+  } catch (err) {
+    console.error('Ошибка копирования в буфер обмена:', err)
+  }
 }
 </script>
