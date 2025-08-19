be3b3bda1f66232cf9ab5df49d12fe28c9e2ea35
Vladislav Chesnov <tazaqsp@mail.ru>
2025-08-19 20:57:09 +0500

    Получение тасков из листов

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,31 @@
 <template>
-  <span>{{ tasks }}</span>
+  <div>
+    <UTextarea v-model="lists"/>
+    <UButton @click="onFetchTasks">Получить задачи</UButton>
+    <span>{{ listIds }}</span>
+    <span>{{ tasks.length }}</span>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useClickUp } from "~/composables/apiClient/useClickUp";
 import type { ITask } from "~/composables/apiClient/types/Task/ITask";
 
+const lists = ref<string>('')
+
 const tasks = ref<ITask[]>([])
 const api = useClickUp()
 
-onMounted(async () => {
-  const response = await api.getTasks('901208117348') || {}
-  if (!response?.data?.tasks) { return }
+const listIds = computed(() => {
+  const regexp = /\/(?:li\/|l\/6-)(\d+)/
 
-  tasks.value = response.data.tasks
+  return lists.value.split('\n').map((line) => line.match(regexp)?.[1]).filter(Boolean)
 })
+
+function onFetchTasks() {
+  listIds.value.forEach(async (listId) => {
+    const response = await api.getTasks(listId as string)
+    tasks.value.push(...response.data.tasks)
+  })
+}
 </script>
