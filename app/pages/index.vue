c48d3cdf62ea3c19d80ebd3410d922c2c88497aa
Vladislav Chesnov <tazaqsp@mail.ru>
2025-08-20 20:33:14 +0500

    Вынес переноску статусов на свою страницу

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,36 +23,18 @@
       </div>
 
       <TaskIdsField :task-ids="tasksIds"/>
-
-      <div class="flex gap-1">
-        <FixedTaskStatuses v-model="fromStatus" class="grow"/>
-        <FixedTaskStatuses v-model="toStatus" class="grow"/>
-        <UButton
-          v-for="step in defaultSteps"
-          :key="step.name"
-          class="hover:opacity-85 cursor-pointer"
-          :style="`background-color: ${step.color};`"
-          @click="onSetStatuses(step)"
-        >{{ step.name }}</UButton>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import FixedTaskStatuses from "~/components/FixedTaskStatuses.vue";
 import TaskIdsField from "~/components/TaskIdsField.vue";
-import { type ITaskStatusStep, useTaskStatuses } from "~/composables/useTaskStatuses";
 import { useTasksStore } from "~/composables/useTasksStore";
 import { useSpaceTree } from "~/composables/spaceTree/useSpaceTree";
 import type { ISpaceTreeNode } from "~/composables/spaceTree/types/ISpaceTreeNode";
 
 const lists = ref<string>('')
-const fromStatus = ref<string>('approved')
-const toStatus = ref<string>('stage')
 const { tasks, tasksIds, tasksLoading, fetchTasks } = useTasksStore()
-
-const { defaultSteps, setTaskStatusesSteps } = useTaskStatuses()
 
 const listIds = computed(() =>
   lists.value
@@ -77,7 +59,5 @@ function onFetchTasks () {
   fetchTasks(listIds.value as string[])
 }
 
-function onSetStatuses (step: ITaskStatusStep) {
-  setTaskStatusesSteps(fromStatus, toStatus, step)
-}
+
 </script>
--- app/pages/tasks-move.vue
+++ app/pages/tasks-move.vue
@@ -1,7 +1,28 @@
 <template>
-
+  <div class="flex flex-col grow gap-1">
+    <div class="flex gap-1">
+      <FixedTaskStatuses v-model="fromStatus" class="grow"/>
+      <FixedTaskStatuses v-model="toStatus" class="grow"/>
+      <UButton
+        v-for="step in defaultSteps"
+        :key="step.name"
+        class="hover:opacity-85 cursor-pointer"
+        :style="`background-color: ${step.color};`"
+        @click="onSetStatuses(step)"
+      >{{ step.name }}</UButton>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import FixedTaskStatuses from "~/components/FixedTaskStatuses.vue";
+import { type ITaskStatusStep, useTaskStatuses } from "~/composables/useTaskStatuses";
 
+const fromStatus = ref<string>('approved')
+const toStatus = ref<string>('stage')
+const { defaultSteps, setTaskStatusesSteps } = useTaskStatuses()
+
+function onSetStatuses (step: ITaskStatusStep) {
+  setTaskStatusesSteps(fromStatus, toStatus, step)
+}
 </script>
